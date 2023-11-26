49463706dac66f397a9e5cbf3c8dde308c495646
Pedro Pimenta <znzdesign@gmail.com>
2023-11-26 02:47:51 +0000

    Update crisp-chat.tsx

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("778541ac-cb4f-448d-b878-c313f1c435e4");
   }, []);
 
   return null;
